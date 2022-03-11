55ecde7f0c49e29f3c652e137cd7b82bb1526831
Brotherteam <dimagilev91@gmail.com>
2022-03-12 04:05:14 +0700

    Lesson 3

--- Les_3/Task_7.py
+++ Les_3/Task_7.py
@@ -5,19 +5,15 @@
 
 import random
 
-
-def quick_sort(array):
-    if len(array) <= 1:
-        return array
-
-    pivot_index = random.randint(0, len(array) - 1)
-    pivot = array[pivot_index]
-    left_array = [number for index, number in enumerate(array) if index != pivot_index and number <= pivot]
-    right_array = [number for index, number in enumerate(array) if index != pivot_index and number > pivot]
-    return quick_sort(left_array) + [pivot] + quick_sort(right_array)
-
-
 random_array = [random.randint(1, 9) for _ in range(10)]
 print(f"random_array={random_array}")
-sorted_array = quick_sort(random_array)
-print(f"два наименьших элемента = {sorted_array[:2]}")
+
+min_1 = min_2 = random_array[0]
+for number in random_array[1:]:
+    if number < min_1:
+        min_2 = min_2
+        min_1 = number
+    elif number < min_2:
+        min_2 = number
+
+print(f"min_1={min_1}, min_2={min_2}")
